03aa9b6109f13a472acc14d2f57a2b212b87e6eb
Nathan <nathanluyg@gmail.com>
2023-09-16 21:41:16 -0700

    Added resume

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/work', title: 'Work' },
   { href: '/projects', title: 'Projects' },
+  { href: "https://docs.google.com/document/d/1XQOXOHMt5eVnGsHpmk26LYIWm2ggeaLfNQJatw5nrA0/edit?usp=sharing", title: 'Resume' },
 //  { href: '/blog', title: 'Blog' },
 //  { href: '/tags', title: 'Tags' },
 // { href: '/about', title: 'About' },
